a17be1b3f1ff610e4bc355ae423dd812b75059dc
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-05-24 14:20:50 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,249 @@
+
+// Program to check the input number is divisible by 3================================
+
+// var number=prompt("Input any number to check its dvisible by 3 or not");
+// if (number%3==0) {
+//     alert("Yes Its divisible by 3.");
+    
+//     }
+    
+//     else{
+//         alert ("No it's not.");
+//     }
+
+
+// Program that checks whether the given input is an even number or an odd number===========
+
+// var random=prompt("Please input number to check whether its odd or even" );
+//  if (random % "2" == 0) {
+//      alert ("Its Even Number");
+    
+//  }
+
+//  else{
+//      alert("Its odd Number");
+//  }
+
+
+// Program to check age===========================================================
+
+// var age=prompt("Please enter your age in number");
+
+// if (age>18) {
+//     alert("You are old enough");
+    
+// } else {
+//     alert("You are too young");
+
+// }
+
+
+
+// Program to take user name===================================================
+
+// var uname="Huzaifa"; 
+
+// var input=prompt("Enter your name...")
+
+// if (input===uname) {
+//     alert("Welcome,Huzaifa your name is same as mine");
+    
+// }
+
+
+// Program to check input is in number,uppercase letter and lowecase letter====================
+
+// var number=prompt("Please input character to check whether its in uppercase,lowercase and number");
+// var character = 's';
+
+
+// if (number*1) {
+//     alert("The type of character is number");
+// }
+
+
+
+// if (character == character.toLowerCase())
+// {
+//   alert ("The character is lowercase");
+// }
+
+// else
+// {
+//     alert ("The character is uppercase");
+// }
+
+
+// Program that take time as input=============================================
+
+// var time=2400;
+// var time= prompt("Please input time (like 1900=7PM)");
+
+// if (time>=0000 && time<1200) {
+//     alert("Good Morning");
+    
+// } 
+
+// else if (time>=1200 && time<1700) {
+//     alert("Good Afternoon");
+
+    
+// }
+
+// else if  (time>=1700 && time<2100) {
+//     alert("Good Evening");
+  
+// }
+
+// else if(time>=2100 && time<2359)   {
+//     alert("Good Night");
+  
+// }
+
+
+
+// Program to check leap year==========================================
+
+// var year= prompt("Please enter year in (yyyy) format to check its leap year or not");
+
+
+//  if ((((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))) {
+//     alert("Its Leap Year")
+    
+// } 
+// else {
+//     alert("Its not leap year")
+    
+// }
+
+
+// Program to check password=======================================
+
+// var pass=123;
+
+// var input=prompt ("Please enter your password");
+ 
+// if (input==pass) {
+//     alert("Your password is correct");
+    
+// } 
+// else {
+//     alert("Your password is incorrect");
+
+// }
+
+
+
+//write a program that adds an else statement to the following script to display You are not Fahad==
+
+// var firstName = "Ali";
+// if (firstName === "Fahad") {
+// document.write("Hello Fahad!");
+// }
+
+// else{
+//     document.write("You are not Fahad");
+// }
+
+
+
+// Program check================================
+
+// var greeting;
+// var hour = 19;
+// if (hour < 18) {
+// greeting = "Good day";
+// document.write(greeting);
+
+// }
+// else{
+// greeting = "Good Evening";
+// document.write(greeting);
+// }
+
+
+// Program to compare two numbers==========================================
+
+// var one= parseInt (prompt("Enter first number"));
+//  var second=parseInt (prompt("Enter Second number"));
+
+//  if (one>second) {
+//     alert( " The greater number between  " + one +" and " + second +" is " + one);
+    
+//  }
+
+//  else if (one==second) {
+//      alert(one + " and " + second + " are equal.");
+    
+// }
+
+//  else{
+//      alert(" The greater number between  " + one + " and " + second +" is " + second);
+//  }
+
+
+
+// Program to check number is positive,negative and zero======================================
+
+// var num=prompt("Please enter a number to check its state");
+
+// if (num==0) {
+//     alert("The number is zero");
+    
+// }
+//  else if (num<0) {
+//     alert("The number is negative");
+    
+// }
+
+// else{
+//     alert("The number is positive");
+
+
+// }
+
+
+// Program to know the current hour================================================
+
+// var input=24;
+
+// var input=prompt("Enter the time in 24 hours format" );
+
+// if (input>6 && input<9) {
+//     alert("Breakfast is served.");
+    
+// }
+
+// else if (input>11 && input<13) {
+//     alert("Time for lunch.");
+    
+// }
+
+// else if (input>17 && input<20) {
+//     alert("Its time for dinner.");
+    
+// }
+
+// else  {
+//     alert("Sorry, you'll have to wait, or go get a snack.");
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
